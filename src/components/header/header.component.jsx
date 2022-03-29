17e20ea13724477879fdde7ba3484a8ba2dad41c
Nabin Dhakal <ndhakal10@gmail.com>
2022-03-29 15:58:30 +0545

    cart icon added and cart-dropdown,also cart reducer implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,27 +4,31 @@ import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from "../../firebase/firebase.utils";
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser,hidden }) => (
     <div className="header">
         <Link to="/" className="logo-container">
             <Logo className='logo' />
         </Link>
-
         <div to="/" className="options">
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/shop'>CONTACT</Link>
             {
                 currentUser ? <div className="option" onClick={() => auth.signOut()}> SIGN OUT</div> : <Link className="option" to='/signin'>SIGN IN </Link>
             }
+            <CartIcon />
         </div>
+        {hidden?null:<CartDropdown />}
 
     </div>
 );
-const mapStateToPropos = state => ({
-    currentUser: state.user.currentUser
+const mapStateToPropos = ({user:{currentUser},cart:{hidden}}) => ({
+     currentUser,
+     hidden
+
 })
 
 
